90658f843064149a9996f9d1255a279ff46bb615
Ethan-cm <ethancm@live.ca>
2021-10-22 16:35:59 -0600

    first look at hash tables

--- Python/twosum.py
+++ Python/twosum.py
@@ -1,6 +1,14 @@
-
+#Learning hashtables, know this problem requires them. I do not claim ownership of this code, this was an exercise in looking at how hash tables work and uses a known solution.
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #step 1: apply a hash function to nums to allow us to lookup numbers efficiently
+        numdic = {} #create dictionary for the 
         
-        print()
+        for i, j  in enumerate(nums): #learned enumerate command, adds a counter to an interable structure
+
+            if j in numdic:
+                return [i,numdic[j]]
+            else:
+                numdic[target - j] = i
+
         
